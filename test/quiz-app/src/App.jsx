2a75e68d201de090287369e86dd753288411c6f6
melomo1997 <143066522+melomo1997@users.noreply.github.com>
2024-01-04 18:47:44 +0100

    check it

--- test/quiz-app/src/App.jsx
+++ test/quiz-app/src/App.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import Quiz from './Components/Quiz/Quiz'
+import React, { useState } from 'react';
+import './Quiz.css';
+import { data } from '../../assets/data';
 
-const App = () => {
+const Quiz = () => {
+  let [index, setIndex] = useState(0); 
+
+  let [question, setQuestion] = useState(data[index]);
+
+  const checkAns = (e, ans) => {
+    if (question.ans === ans) {
+      e.target.classList.add("correct");
+    } else {
+      e.target.classList.add("wrong");
+    }
+  };
+
   return (
-    <>
-      <Quiz/>
-    </>
-  )
-}
+    <div className='container'>
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>{index + 1}. {question.question}</h2>
+      <div className='box'>
+        <ul>
+          <li onClick={(e) => { checkAns(e, 1) }}>{question.option1}</li>
+          <li onClick={(e) => { checkAns(e, 2) }}>{question.option2}</li>
+        </ul>
+        <ul>
+          <li onClick={(e) => { checkAns(e, 3) }}>{question.option3}</li>
+          <li onClick={(e) => { checkAns(e, 4) }}>{question.option4}</li>
+        </ul>
+      </div>
+      <button>Next</button>
+      <div className='index'>1 of 3</div>
+    </div>
+  );
+};
 
-export default App
+export default Quiz;
